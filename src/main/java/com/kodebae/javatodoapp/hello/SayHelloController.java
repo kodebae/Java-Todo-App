d2e05e7f13ac213043380746bd84f98207e16e1e
kodebae <heykarmendurbin@gmail.com>
2023-04-25 01:14:57 -0400

    practicing with Spring Boot String Buffer

--- src/main/java/com/kodebae/javatodoapp/hello/SayHelloController.java
+++ src/main/java/com/kodebae/javatodoapp/hello/SayHelloController.java
@@ -7,10 +7,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class SayHelloController {
     // get a response from
-    @RequestMapping("say-hello")
+    @RequestMapping("say-hello") // http://localhost8080/say-hello
     @ResponseBody// map a URL to this method
     public String sayHello() {
-        return "Hello Panda, I love you!";
+        return "Hello World!";
     }
-
+    @RequestMapping("say-hello-html") // http://localhost8080/say-hello
+    @ResponseBody// map a URL to this method
+    public String sayHelloHTML() {
+        StringBuffer sb = new StringBuffer();
+            sb.append("<html>");
+            sb.append("<body>");
+            sb.append("Saying hello with HTML");
+            sb.append("</body>");
+            sb.append("</html>");
+        return sb.toString();
+    }
 }
