f80056e3f0dec407523e3b31771d4c9a6271a70f
kodebae <heykarmendurbin@gmail.com>
2023-04-25 00:02:32 -0400

    intial commit

---
+++ src/main/java/com/kodebae/javatodoapp/JavatodoappApplication.java
@@ -0,0 +1,13 @@
+package com.kodebae.javatodoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavatodoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavatodoappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kodebae/javatodoapp/JavatodoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodebae.javatodoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavatodoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
